f280ad07b435acba107f63802ccffdb2be649d30
Tony Lockhart <tony.lockhart@ymail.com>
2021-12-12 19:48:47 -0500

    chore: Split out Posts into components.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,9 @@
 import Post from "./components/Post/Post";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Posts from "./components/Posts/Posts";
 
 function App() {
-  const initData = [
-    {
-      userId: 1,
-      id: 1,
-      title:
-        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-    },
-    {
-      userId: 1,
-      id: 2,
-      title: "qui est esse",
-      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-    },
-  ];
 
   async function getData() {
     let response = await axios.get("https://jsonplaceholder.typicode.com/posts");
@@ -52,19 +38,9 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-6">
-          <h1>All Posts</h1>
-          {posts.map((post) => (
-            <Post {...post} key={post.id} />
-          ))}
-        </div>
+        <Posts posts={posts} title="All Posts"/>
         {/* Duplicate */}
-        <div className="col-6">
-        <h1>My Posts</h1>
-          {myPosts.map((post) => (
-            <Post {...post} key={post.id} />
-          ))}
-        </div>
+        <Posts posts={myPosts} title="My Posts"/>
       </div>
     </div>
   );
